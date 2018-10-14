d0856196e35a791c619a6d51cc6aff2f25a95688
Aasem Ahmad <engh.asem@gmail.com>
2018-10-14 19:14:22 +0200

    initial commit

---
+++ application/src/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+int sub (int, int);
+int sum(int a, int b) {
+	return a + b;
+}
+
+int main() {
+	std::cout << "hello world \n";
+	std:cout << "here are some changes \n"; 
+	
+	
+}
+int sub(int a, int b) { 
+return a - b;
+}
